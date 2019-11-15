aef551a7088f5b5ff58127c26f91366ec2c9e57a
alexfour <46669408+alexfour@users.noreply.github.com>
2019-11-15 22:54:33 +0200

    Added a way to track a global hotkeys states (Down and Up) and made the screenshot size to match initial and end mouse XY coordinates

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Screenshot";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Save path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Image prefix";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(44, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(44, 185);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Delay";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(499, 44);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(84, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Choose reso";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(499, 79);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Static reso";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(696, 79);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(590, 79);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(44, 277);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Shortcut";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "DocuShotter";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,144 @@ namespace DocuShotter
 {
     public partial class Form1 : Form
     {
+
+        private bool isPressed = false; //Track if global hotkey is currently down
+
+        private int initialX = 0;       //Initial X coordinate of the screenshot
+        private int initialY = 0;       //Initial Y coordinate of the screenshot
+        private int endX = 0;           //Destination X coordinate of the screenshot
+        private int endY = 0;           //Destination Y coordinate of the screenshot
+        private int shotWidth = 0;      //Width of the screenshot area
+        private int shotHeight = 0;     //Height of the screenshot area
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
+
         public Form1()
         {
             InitializeComponent();
+            this.KeyPreview = true;
+
+            Boolean success = Form1.RegisterHotKey(this.Handle, this.GetType().GetHashCode(), 0x0000, 0x42);    //Set hotkey as 'b' and try to register a global hotkey
+            if (success == true)
+                Console.WriteLine("Hotkey registered");
+            else
+                Console.WriteLine("Error registering hotkey");
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = System.Windows.Forms.Cursor.Position.X + " " + System.Windows.Forms.Cursor.Position.Y;
+        }
+
+        /// <summary>
+        /// Main function for taking the screenshot. Creates a Bitmap and sets it's size to the parameters width and height. 
+        /// Afterwards takes a screenshot of the area and saves it.
+        /// </summary>
+        /// <param name="width">Width of the screenshot area</param>
+        /// <param name="height">Height of the screenshot area</param>
+        private void TakeScreenShot(int width, int height)
+        {
+            Bitmap bmp = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(initialX, initialY, 0, 0, new Size(bmp.Width, bmp.Height));
+                bmp.Save("screenshot.png");  // saves the image
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A && !isPressed) //TODO REMOVE e.KeyCode == Keys.A && 
+            {
+                initialX = System.Windows.Forms.Cursor.Position.X;
+                initialY = System.Windows.Forms.Cursor.Position.Y;
+                textBox1.Text = System.Windows.Forms.Cursor.Position.X + " " + System.Windows.Forms.Cursor.Position.Y;
+                isPressed = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A && isPressed)
+            {
+                textBox2.Text = System.Windows.Forms.Cursor.Position.X + " " + System.Windows.Forms.Cursor.Position.Y;
+                endX = System.Windows.Forms.Cursor.Position.X;
+                endY = System.Windows.Forms.Cursor.Position.Y;
+                shotWidth = Math.Abs(initialX - endX);
+                shotHeight = Math.Abs(initialY - endY);
+                textBox3.Text = shotWidth + " " + shotHeight;
+                isPressed = false;
+
+                TakeScreenShot(shotWidth, shotHeight);
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312 && !isPressed)
+            {
+                initialX = System.Windows.Forms.Cursor.Position.X;
+                initialY = System.Windows.Forms.Cursor.Position.Y;
+                Console.Write("pressed" + isPressed);
+                isPressed = true;
+                InitTimer();
+                //MessageBox.Show("Catched");//You can replace this statement with your desired response to the Hotkey.
+            }
+            base.WndProc(ref m);
+        }
+
+        //Delete hotkey when form is closed
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Boolean success = Form1.UnregisterHotKey(this.Handle, this.GetType().GetHashCode());//Set hotkey as 'b'
+            if (success == true)
+                MessageBox.Show("Success");
+            else
+                MessageBox.Show("Error");
+        }
+
+        private Timer timer2;
+        public void InitTimer()
+        {
+            Console.WriteLine("inittimer");
+            timer2 = new Timer();
+            timer2.Tick += new EventHandler(timer2_Tick);
+            timer2.Interval = 16; // in miliseconds
+            timer2.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Console.WriteLine("timertick");
+            short keyState = GetAsyncKeyState(0x42);
+
+            //Check if the MSB is set. If so, then the key is pressed.
+            bool prntScrnIsPressed = ((keyState >> 15) & 0x0001) == 0x0001;
+
+            if (prntScrnIsPressed)
+            {
+                Console.WriteLine("pressed still");
+            }
+            else
+            {
+                Console.WriteLine("released" + isPressed);//TODO Execute client code...
+                timer2.Stop();
+
+                isPressed = false;
+                endX = System.Windows.Forms.Cursor.Position.X;
+                endY = System.Windows.Forms.Cursor.Position.Y;
+                shotWidth = Math.Abs(initialX - endX);
+                shotHeight = Math.Abs(initialY - endY);
+                TakeScreenShot(shotWidth, shotHeight);
+            }
+
         }
     }
 }
