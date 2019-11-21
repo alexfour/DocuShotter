8f46723dbb9d542ce08654d5ce8e25f2aeee2e10
alexfour <46669408+alexfour@users.noreply.github.com>
2019-11-21 18:26:50 +0200

    Fixed graphics being show on screenshot Improved documentation

--- DrawForm.cs
+++ DrawForm.cs
@@ -21,13 +21,19 @@ namespace DocuShotter
 
         private Form1 formis;           //Object holder for the main Form1
         private Timer mainTimer;        //Timer object that fires whenever this form is show and causes invalidates
-        Timer hidetimer;                //Timer that hides the form after it is no longer needed
+        Timer hidetimer, shottimer;     //Timer that hides the form after it is no longer needed
+
+        private bool drawanything;      //Indicates that graphics drawn on screen need to be hidden
 
         private bool mousePressed,released = false;
 
         [DllImport("User32.dll")]
         private static extern short GetAsyncKeyState(int vKey);
 
+        /// <summary>
+        /// Constructor for DrawForm where the fullscreen picturebox is initialized
+        /// </summary>
+        /// <param name="f"></param>
         public DrawForm(Form1 f)
         {
             formis = f;
@@ -58,6 +64,11 @@ namespace DocuShotter
             TopMost = true; // This will bring your window in front of all other windows including the taskbar
         }
 
+        /// <summary>
+        /// Setup function called when user has started the sceenshotting process.
+        /// Choose from one of two screenshotting modes: instantenous or draw mode
+        /// </summary>
+        /// <param name="mode">Drawing mode: 0 is instantenous, 1 is draw mode</param>
         public void Setup(int mode)
         {
             pictureBox1.ImageLocation = AppDomain.CurrentDomain.BaseDirectory + "background";
@@ -75,6 +86,7 @@ namespace DocuShotter
                 mousePressed = true;
             }
             InitTimer();
+            drawanything = true;
         }
 
         private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
@@ -95,6 +107,7 @@ namespace DocuShotter
         {
             if (mode == 1)
             {
+                drawanything = false;
                 endX = System.Windows.Forms.Cursor.Position.X;
                 endY = System.Windows.Forms.Cursor.Position.Y;
                 CalculateDimensions();
@@ -102,24 +115,17 @@ namespace DocuShotter
                 pictureBox1.ImageLocation = null;
                 mainTimer.Stop();
                 pictureBox1.Update();
-                formis.TakeScreenShot(shotWidth, shotHeight, initialX, initialY);
-                formis.isPressed = false;
 
-                hidetimer = new Timer();
-                hidetimer.Tick += new EventHandler(Hidetimer_Tick);
-                hidetimer.Interval = 15; // in miliseconds
-                hidetimer.Start();
+                shottimer = new Timer();
+                shottimer.Tick += new EventHandler(ShotTimer_Tick);
+                shottimer.Interval = 100; // in miliseconds
+                shottimer.Start();
             }
         }
 
-        private void Hidetimer_Tick(object sender, EventArgs e)
-        {
-            this.Hide();
-            hidetimer.Stop();
-            hidetimer.Dispose();
-            released = false;
-        }
-
+        /// <summary>
+        /// Calculates the screenshot width and height using initial and end XY coordinates
+        /// </summary>
         private void CalculateDimensions()
         {
             if (endX < initialX)
@@ -138,6 +144,9 @@ namespace DocuShotter
             shotHeight = Math.Abs(initialY - endY);
         }
 
+        /// <summary>
+        /// Timer function that starts the redrawing process via Invalidate
+        /// </summary>
         public void InitTimer()
         {
             mainTimer.Tick += new EventHandler(MainTimer_Tick);
@@ -148,6 +157,25 @@ namespace DocuShotter
         private void MainTimer_Tick(object sender, EventArgs e)
         {
             pictureBox1.Invalidate();
+
+            /*//Check for escape
+            short esckeyState = GetAsyncKeyState(0x1B);
+            bool esckeyIsPressed = ((esckeyState >> 15) & 0x0001) == 0x0001;
+
+            if (esckeyIsPressed)
+            {
+                mousePressed = false;
+                released = true;
+                pictureBox1.Update();
+                mainTimer.Stop();
+
+                formis.isPressed = false;
+                esckeyIsPressed = false;
+                hidetimer = new Timer();
+                hidetimer.Tick += new EventHandler(Hidetimer_Tick);
+                hidetimer.Interval = 15; // in miliseconds
+                hidetimer.Start();
+            }*/
 
             if (mode == 0 && !released)
             {
@@ -161,6 +189,7 @@ namespace DocuShotter
 
                 if (!hotkeyScrnIsPressed | !ctrlScrnIsPressed)
                 {
+                    drawanything = false;
                     endX = System.Windows.Forms.Cursor.Position.X;
                     endY = System.Windows.Forms.Cursor.Position.Y;
                     CalculateDimensions();
@@ -170,42 +199,80 @@ namespace DocuShotter
                     pictureBox1.ImageLocation = null;
                     mainTimer.Stop();
 
-                    formis.TakeScreenShot(shotWidth, shotHeight, initialX, initialY);
-                    formis.isPressed = false;
 
-                    hidetimer = new Timer();
-                    hidetimer.Tick += new EventHandler(Hidetimer_Tick);
-                    hidetimer.Interval = 15; // in miliseconds
-                    hidetimer.Start();
+                    shottimer = new Timer();
+                    shottimer.Tick += new EventHandler(ShotTimer_Tick);
+                    shottimer.Interval = 15; // in miliseconds
+                    shottimer.Start();
+
                 }
             }
         }
 
+        /// <summary>
+        /// hideTimer tick function that hides the form in 15 milliseconds from when screenshotting process is complete
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Hidetimer_Tick(object sender, EventArgs e)
+        {
+            this.Hide();
+            hidetimer.Stop();
+            hidetimer.Dispose();
+            released = false;
+        }
+
+        /// <summary>
+        /// Exists to give the graphics time to clear the graphics being draw on the screen so that they wont show up in the screenshot
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ShotTimer_Tick(object sender, EventArgs e)
+        {
+            formis.TakeScreenShot(shotWidth, shotHeight, initialX, initialY);
+            formis.isPressed = false;
+            shottimer.Stop();
+            shottimer.Dispose();
+
+            hidetimer = new Timer();
+            hidetimer.Tick += new EventHandler(Hidetimer_Tick);
+            hidetimer.Interval = 15; // in miliseconds
+            hidetimer.Start();
+        }
+
+        /// <summary>
+        /// Paint event where the crosshairs and data string are drawn onto the screen and then hidden when user initiates a screenshot
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void pictureBox1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
         {
             var relativePoint = this.PointToClient(Cursor.Position);
-            // Create a local version of the graphics object for the PictureBox.
-            Graphics g = e.Graphics;
-            g.FillRectangle(new SolidBrush(Color.White), new Rectangle(relativePoint.X + 0, relativePoint.Y - 15, 60, 15));
-            g.DrawString(relativePoint.X + "-" + relativePoint.Y, fnt, new SolidBrush(Color.Black), relativePoint.X + 0, relativePoint.Y - 15, System.Drawing.StringFormat.GenericDefault);
+            Graphics g = e.Graphics;        // Create a local version of the graphics object for the PictureBox.
 
-            if (mousePressed)
+            if (drawanything)
             {
-                g.FillRectangle(new SolidBrush(Color.White), new Rectangle(relativeX + 0, relativeY - 15, 60, 15));
-                g.DrawString(relativeX + "-" + relativeY, fnt, new SolidBrush(Color.Black), relativeX + 0, relativeY - 15, System.Drawing.StringFormat.GenericDefault);
+                g.FillRectangle(new SolidBrush(Color.White), new Rectangle(relativePoint.X + 0, relativePoint.Y - 15, 60, 15));
+                g.DrawString(relativePoint.X + "-" + relativePoint.Y, fnt, new SolidBrush(Color.Black), relativePoint.X + 0, relativePoint.Y - 15, System.Drawing.StringFormat.GenericDefault);
 
-                shotWidth = Math.Abs(initialX - System.Windows.Forms.Cursor.Position.X);
-                shotHeight = Math.Abs(initialY - System.Windows.Forms.Cursor.Position.Y);
-                g.FillRectangle(new SolidBrush(Color.White), new Rectangle(relativePoint.X + 0, relativePoint.Y + 20, 80, 30));
-                g.DrawString("Width = " + shotWidth, fnt, new SolidBrush(Color.Black), relativePoint.X + 0, relativePoint.Y + 20, System.Drawing.StringFormat.GenericDefault);
-                g.DrawString("Height = " + shotHeight, fnt, new SolidBrush(Color.Black), relativePoint.X + 0, relativePoint.Y + 35, System.Drawing.StringFormat.GenericDefault);
+                if (mousePressed)
+                {
+                    g.FillRectangle(new SolidBrush(Color.White), new Rectangle(relativeX + 0, relativeY - 15, 60, 15));
+                    g.DrawString(relativeX + "-" + relativeY, fnt, new SolidBrush(Color.Black), relativeX + 0, relativeY - 15, System.Drawing.StringFormat.GenericDefault);
 
-                g.DrawLine(System.Drawing.Pens.Red, relativeX, 0, relativeX, 5000);
-                g.DrawLine(System.Drawing.Pens.Red, 0, relativeY, 5000, relativeY);
+                    shotWidth = Math.Abs(initialX - System.Windows.Forms.Cursor.Position.X);
+                    shotHeight = Math.Abs(initialY - System.Windows.Forms.Cursor.Position.Y);
+                    g.FillRectangle(new SolidBrush(Color.White), new Rectangle(relativePoint.X + 0, relativePoint.Y + 20, 85, 30));
+                    g.DrawString("Width = " + shotWidth, fnt, new SolidBrush(Color.Black), relativePoint.X + 0, relativePoint.Y + 20, System.Drawing.StringFormat.GenericDefault);
+                    g.DrawString("Height = " + shotHeight, fnt, new SolidBrush(Color.Black), relativePoint.X + 0, relativePoint.Y + 35, System.Drawing.StringFormat.GenericDefault);
+
+                    g.DrawLine(System.Drawing.Pens.Red, relativeX, 0, relativeX, 5000);
+                    g.DrawLine(System.Drawing.Pens.Red, 0, relativeY, 5000, relativeY);
+                }
+
+                g.DrawLine(System.Drawing.Pens.Red, relativePoint.X, 0, relativePoint.X, 5000);
+                g.DrawLine(System.Drawing.Pens.Red, 0, relativePoint.Y, 5000, relativePoint.Y);
             }
-
-            g.DrawLine(System.Drawing.Pens.Red, relativePoint.X, 0, relativePoint.X, 5000);
-            g.DrawLine(System.Drawing.Pens.Red, 0, relativePoint.Y, 5000, relativePoint.Y);
         }
     }
 }
